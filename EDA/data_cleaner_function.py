a58d0cec54b224f26a9a3bb470df2b78147f841f
plawansaifad <89628456+plawansaifad@users.noreply.github.com>
2021-11-11 16:33:03 +0700

    Create data_cleaner_function.py

---
+++ EDA/data_cleaner_function.py
@@ -0,0 +1,60 @@
+###############################################################################
+
+def missing(df):
+    missing_number = df.isnull().sum().sort_values(ascending=False)
+    missing_percent = (df.isnull().sum()/df.isnull().count()).sort_values(ascending=False)
+    missing_values = pd.concat([missing_number, missing_percent], axis=1, keys=['Missing_Number', 'Missing_Percent'])
+    return missing_values
+
+def missing_values(df):
+    return missing(df)[missing(df)['Missing_Number']>0]
+
+###############################################################################
+
+def first_looking(df):
+    print(colored("Shape:", attrs=['bold']), df.shape,'\n',
+          f"There is ", df.shape[0], " observation and ", df.shape[1], " columns in the dataset.", '\n',
+          colored('-'*79, 'red', attrs=['bold']),
+          colored("\nInfo:\n", attrs=['bold']), sep='')
+    print(df.info(), '\n', 
+          colored('-'*79, 'red', attrs=['bold']), sep='')
+    print(colored("Number of Uniques:\n", attrs=['bold']), df.nunique(),'\n',
+          colored('-'*79, 'red', attrs=['bold']), sep='')
+    print(colored("Missing Values:\n", attrs=['bold']), missing_values(df),'\n', 
+          colored('-'*79, 'red', attrs=['bold']), sep='')
+    print(colored("All Columns:", attrs=['bold']), list(df.columns),'\n', 
+          colored('-'*79, 'red', attrs=['bold']), sep='')
+
+    df.columns= df.columns.str.lower().str.replace('&', '_').str.replace(' ', '_')
+
+    print(colored("Columns after rename:", attrs=['bold']), list(df.columns),'\n',
+              colored('-'*79, 'red', attrs=['bold']), sep='')
+
+def duplicate_values(df):
+    duplicate_values = df.duplicated(subset=None, keep='first').sum()
+    if duplicate_values > 0:
+        df.drop_duplicates(keep='first', inplace=True)
+        print(duplicate_values, colored("duplicates were dropped", attrs=['bold']),'\n',
+              colored('-'*79, 'red', attrs=['bold']), sep='')
+    else:
+        print(colored("No duplicates", attrs=['bold']),'\n',
+              colored('-'*79, 'red', attrs=['bold']), sep='')
+        
+def drop_columns(df, drop_columns):
+    if drop_columns !=[]:
+        df.drop(drop_columns, axis=1, inplace=True)
+        print(drop_columns, 'were dropped')
+    else:
+        print(colored('We will now check the missing values and if necessary drop some columns!!!', attrs=['bold']),'\n',
+              colored('-'*79, 'red', attrs=['bold']), sep='')
+        
+def drop_null(df, limit):
+    print('Shape:', df.shape)
+    for i in df.isnull().sum().index:
+        if (df.isnull().sum()[i]/df.shape[0]*100)>limit:
+            print(df.isnull().sum()[i], 'percent of', i ,'null and were dropped')
+            df.drop(i, axis=1, inplace=True)
+            print('new shape:', df.shape)
+        else:
+            print(df.isnull().sum()[i], '%, percentage of missing values of', i ,'less than limit', limit, '%, so we will keep it.')
+    print('New shape after missing value control:', df.shape)
